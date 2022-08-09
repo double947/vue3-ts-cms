6ec73468ad56f3ff7cf86ca58c24b58ba3dd7fa2
double947 <1774387663@qq.com>
2022-08-09 17:46:29 +0800

    chore(store): 集成store

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <div class="app">
+    <div class="">{{ $store.state.name }}</div>
     <router-link to="/login">登录</router-link>
     <router-link to="/main">首页</router-link>
     <router-view></router-view>
