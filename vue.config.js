b253b62005c51edd18394fba66b8a6866e7d800d
double947 <1774387663@qq.com>
2023-10-17 17:52:41 +0800

    build(baseurl): 更新baseUrl

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://152.136.185.210:5000',
+        target: 'http://codercba.com:5000/',
         pathRewrite: {
           '^/api': ''
         },
