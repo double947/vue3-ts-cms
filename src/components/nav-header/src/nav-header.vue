a90fcce7c20b7a20e9a5e634e436bf2a5168ee23
double947 <1774387663@qq.com>
2023-10-20 14:37:05 +0800

    feat(nav-header): nav-header用户控制布局

--- src/components/nav-header/src/nav-header.vue
+++ src/components/nav-header/src/nav-header.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="nav-header">
+  <div class="nav-header flex justify-between w-[100%]">
     <el-icon :size="25">
       <Fold @click="handleFlodChange" v-if="isFlod" />
       <Expand @click="handleFlodChange" v-else />
     </el-icon>
+    <div class="content flex flex-[1] justify-between items-center">
+      <div>面包屑</div>
+      <user-info />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
+import UserInfo from './user-info.vue'
 
 export default defineComponent({
+  components: { UserInfo },
   emits: ['flodChange'],
   setup(props, { emit }) {
     const isFlod = ref(false)
